e2296dbba7f0577afb678be8229d364003a62632
gabebider <gabebider2024@u.northwestern.edu>
2023-01-13 13:26:57 -0600

    Scrapes for actors as well

--- scraper.py
+++ scraper.py
@@ -23,6 +23,32 @@ def findMovies():
                 movies[link.text] = 1
 
     print(movies)
-    print(len(movies))
+    return movies
+
+
+
+def findActors():
+    # this also includes actresses
+    actors = {}
+    for start in range(1, 10001, 100):
+        # WORK IN Progress
+        url = f"https://www.imdb.com/search/name/?birth_date=1900-01-01,2022-12-31&count=100&start={start}&ref_=rlm"
+        
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, "html.parser")
+
+        hyperlinks = soup.find_all("a", href=re.compile("name/nm"))
+
+        for link in hyperlinks:
+            if link.text != "" and link.text != "\n" and link.text != " \n" and link.text != "  \n":
+                # remove the \n at end of name
+                actor_name = link.text[:-1]
+                # strip extra spaces
+                actors[actor_name.strip()] = 1
+    
+    print(actors)
+    return actors
+
 
 findMovies()
+findActors()
