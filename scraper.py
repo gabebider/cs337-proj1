1c384e37a8828017c4a2ee64aaf4e46680fa6630
gabebider <gabebider2024@u.northwestern.edu>
2023-01-13 13:19:01 -0600

    Scrapes a list of 9595 movies

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ import re
 
 def findMovies():
 
+    movies = {}
     for start in range(1, 10001, 250):
         url = f"https://www.imdb.com/search/title/?release_date=1900-01-01,2022-12-31&view=simple&sort=num_votes,desc&count=250&start={start}&ref_=adv_nxt"
         # query the page
@@ -16,11 +17,12 @@ def findMovies():
         hyperlinks = soup.find_all("a", href=re.compile("title/tt"))
 
         # print(hyperlinks)
-        movies = {}
+        
         for link in hyperlinks:
             if link.text != "" and link.text != "\n" and link.text != " \n" and link.text != "  \n":
                 movies[link.text] = 1
-        
+
     print(movies)
+    print(len(movies))
 
 findMovies()
