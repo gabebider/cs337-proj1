d828873af260e0106f12f0cb9878c31f1e3c6d36
isaacbmiller <isaacbmiller@gmail.com>
2023-01-16 17:53:27 -0600

    Add golden globe dataset

--- PhaseOne.py
+++ PhaseOne.py
@@ -1,21 +1,32 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
 import json
 import re
+import nltk
+nltk.download('vader_lexicon')
 
 data = json.load(open('gg2013.json'))
 
 djangoCount = 0
 argoCount = 0
+printCount = 0
+sia = SentimentIntensityAnalyzer()
+
+movieNameDict = {"life of pi": 0, "lincoln": 0,
+                 "zero dark thirty": 0, "django unchained": 0, "argo": 0}
 
 for item in data:
     text = item['text'].lower()
+    for movie in movieNameDict:
+        if re.match(r".*"+movie+".*", text):
+            # print(item['text'])
+            # if sia.polarity_scores(item['text'])['compound'] > 0.8:
+            # print(item['text'])
+            # printCount += 1
+            movieNameDict[movie] += 1
 
-    if re.match(r".*django unchained.*", text):
-        # print(item['text'])
-        djangoCount += 1
-    
-    if re.match(r".*argo.*", text):
-        # print(item['text'])
-        argoCount += 1
 
-print(f"djangoCount: {djangoCount}")
-print(f"argoCount: {argoCount}")
+print(f"Life of Pi: {movieNameDict['life of pi']}")
+print(f"Lincoln: {movieNameDict['lincoln']}")
+print(f"Zero Dark Thirty: {movieNameDict['zero dark thirty']}")
+print(f"Django Unchained: {movieNameDict['django unchained']}")
+print(f"Argo: {movieNameDict['argo']}")
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # this is impossible wtf
+
 yuh
+
+## Plan
+
+- [ ] winner, given award names and nominees
+- [ ] winners, given award names and nominees
+- [ ] nominees, given award names
+- [ ] award names, given ceremony
+- [ ] presenter, given award names
+- [ ] host, given ceremony
