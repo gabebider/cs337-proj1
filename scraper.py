2f12d48fa1aba5d309d2fd8757384f7466c373ad
gabebider <gabebider2024@u.northwestern.edu>
2023-01-16 17:25:45 -0600

    Include CSVs for Actors and Movies

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import csv
 
 
 def findMovies():
 
-    movies = {}
+    movies = []
     for start in range(1, 10001, 250):
         url = f"https://www.imdb.com/search/title/?release_date=1900-01-01,2022-12-31&view=simple&sort=num_votes,desc&count=250&start={start}&ref_=adv_nxt"
         # query the page
@@ -20,16 +21,16 @@ def findMovies():
         
         for link in hyperlinks:
             if link.text != "" and link.text != "\n" and link.text != " \n" and link.text != "  \n":
-                movies[link.text] = 1
+                movies.append(link.text)
 
-    # print(movies)
+
     return movies
 
 
 
 def findActors():
     # this also includes actresses
-    actors = {}
+    actors = []
     for start in range(1, 10001, 100):
         # WORK IN Progress
         url = f"https://www.imdb.com/search/name/?birth_date=1900-01-01,2022-12-31&count=100&start={start}&ref_=rlm"
@@ -44,11 +45,18 @@ def findActors():
                 # remove the \n at end of name
                 actor_name = link.text[:-1]
                 # strip extra spaces
-                actors[actor_name.strip()] = 1
+                actors.append(actor_name.strip())
     
-    # print(actors)
     return actors
 
 
-# findMovies()
-# findActors()
+movies = findMovies()
+actors = findActors()
+
+with open('movies.csv', 'w', newline='') as myfile:
+     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+     wr.writerow(movies)
+
+with open('actors.csv', 'w', newline='') as myfile:
+     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+     wr.writerow(actors)
