c22ea729a33986d5697127e5a1c070257e795937
isaacbmiller <isaacbmiller@gmail.com>
2023-01-16 18:13:01 -0600

    WIP testing function

---
+++ Nominees.py
@@ -0,0 +1,38 @@
+
+import csv
+
+
+def getNominees():
+    # Open the CSV file and read the data into a list
+    with open("golden_globe_awards.csv", "r") as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+    # Filter the data to only include films from 2013
+    filtered_data = [row for row in data if row[1] == "2013"]
+
+    # Create a dictionary to store the award information
+    awards = {}
+
+    # Loop through the filtered data and add the award information to the dictionary
+    for row in filtered_data:
+        category = row[3].lower()
+        nominee = row[4].lower().strip()
+        film = row[5].lower().replace("\t", "").strip()
+        win = row[6].lower()
+
+        # Check if the award already exists in the dictionary
+        if category not in awards:
+            # If it doesn't, create a new entry for the award
+            awards[category] = {"winner": None, "nominees": []}
+
+        # Add the nominee information to the dictionary
+        awards[category]["nominees"].append(
+            {"nominee": nominee, "film": film})
+
+        # If the nominee won, update the winner property
+        if win == "True":
+            awards[category]["winner"] = {"nominee": nominee, "film": film}
+
+    # print(awards)
+    return awards
--- PhaseOne.py
+++ PhaseOne.py
@@ -1,7 +1,11 @@
+import pprint
 from nltk.sentiment import SentimentIntensityAnalyzer
 import json
 import re
 import nltk
+import csv
+
+from Nominees import getNominees
 nltk.download('vader_lexicon')
 
 data = json.load(open('gg2013.json'))
@@ -11,22 +15,29 @@ argoCount = 0
 printCount = 0
 sia = SentimentIntensityAnalyzer()
 
-movieNameDict = {"life of pi": 0, "lincoln": 0,
-                 "zero dark thirty": 0, "django unchained": 0, "argo": 0}
-
-for item in data:
-    text = item['text'].lower()
-    for movie in movieNameDict:
-        if re.match(r".*"+movie+".*", text):
-            # print(item['text'])
-            # if sia.polarity_scores(item['text'])['compound'] > 0.8:
-            # print(item['text'])
-            # printCount += 1
-            movieNameDict[movie] += 1
+awards = getNominees()
 
 
-print(f"Life of Pi: {movieNameDict['life of pi']}")
-print(f"Lincoln: {movieNameDict['lincoln']}")
-print(f"Zero Dark Thirty: {movieNameDict['zero dark thirty']}")
-print(f"Django Unchained: {movieNameDict['django unchained']}")
-print(f"Argo: {movieNameDict['argo']}")
+def testAward(award):
+    nomineeNameDict = {}
+    for nominee in award['nominees']:
+        nomineeNameDict[nominee] = 0
+    for item in data:
+        text = item['text'].lower()
+        for nominee in nomineeNameDict:
+            if re.match(r".*"+nominee+".*", text):
+                nomineeNameDict[nominee] += 1
+
+    mostPopular = max(nomineeNameDict, key=nomineeNameDict.get)
+    # print(f"Most popular nominee for {award}: {mostPopular}")
+    if award['winner']['nominee'] == mostPopular:
+        return True
+    else:
+        return False
+
+
+results = []
+for award in awards:
+    results.append(testAward(award))
+
+print(sum(results)/len(results))
