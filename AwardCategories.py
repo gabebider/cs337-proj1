e4cbe339dc8368021033e274ece995a6227932f3
Eli Barlow <exbarlow@gmail.com>
2023-02-04 17:52:05 -0600

    starting on noun identification

--- AwardCategories.py
+++
@@ -1,6 +0,0 @@
-class AwardCategories:
-    def __init__(self, award_categories=[]):
-        self.award_categories = award_categories
-
-    def GetAwardCategories(self):
-        return self.award_categories
