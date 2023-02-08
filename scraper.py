e9d39d9590ff78c375723c8fbf819ee1a92df850
Isaac Miller <isaacbmiller@gmail.com>
2023-02-08 16:14:43 -0600

    Scrape tv shows and add to movies.csv

--- scraper.py
+++ scraper.py
@@ -27,7 +27,26 @@ def findMovies():
 
     return movies
 
+def findTV():
+    # https://www.imdb.com/search/title/?title_type=tv_movie,tv_series,tv_miniseries&release_date=2000-01-01,2022-12-31&sort=num_votes,desc&count=250
+    shows = []
+    for start in range(1, 2500, 250):
+        # url = f"https://www.imdb.com/search/title/?release_date=1900-01-01,2022-12-31&view=simple&sort=num_votes,desc&count=250&start={start}&ref_=adv_nxt"
+        url = f"https://www.imdb.com/search/title/?title_type=tv_movie,tv_series,tv_miniseries&release_date=2000-01-01,2022-12-31&sort=num_votes,desc&count=250&start={start}&ref_=adv_nxt"
+        # query the page
+        html = requests.get(url)
+        # parse the html using beautiful soup and store in variable `soup`
+        soup = BeautifulSoup(html.text, "html.parser")
 
+        # hyperlinks = soup.select("a[href*=title]")
+        hyperlinks = soup.find_all("a", href=re.compile("title/tt"))
+
+        # print(hyperlinks)
+        
+        for link in hyperlinks:
+            if link.text != "" and link.text != "\n" and link.text != " \n" and link.text != "  \n" and link.text != "X":
+                shows.append(link.text)
+    return shows
 
 def findPeople():
     # this also includes actresses
@@ -65,9 +84,23 @@ def findPeople():
     return people
 
 
-people = findPeople()
+# people = findPeople()
+tv = findTV()
 
 
-with open('people.csv', 'w', newline='') as myfile:
-     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-     wr.writerow(people)
+# with open('people.csv', 'w', newline='') as myfile:
+#      wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+#      wr.writerow(people)
+
+# merge TV into movies.csv if the entry is not already in movies.csv
+with open('movies.csv', 'r') as file:
+    reader = csv.reader(file)
+    movies = list(reader)[0]
+    for show in tv:
+        if show not in movies:
+            movies.append(show)
+
+
+with open('movies.csv', 'w', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(movies)
