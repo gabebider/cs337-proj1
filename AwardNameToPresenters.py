3b0f105ec899c96c455ae76f0c8ae84d6a2bedeb
Isaac Miller <isaacbmiller@gmail.com>
2023-02-08 19:16:03 -0600

    Presneters

--- AwardNameToPresenters.py
+++ AwardNameToPresenters.py
@@ -21,6 +21,16 @@ from TweetsNearMedian import TweetsNearMedian
 # Step 4: Use NER to find the names of the presenters
 # Step 5: Return the names of the presenters
 
+# CALL MEEEEEE
+def presenters_for_award_alias(aliases, tweets, bad_names):
+    
+    new_tweets = tweets_for_time_interval(tweets, aliases, min_before=2, min_after=1)
+    new_tweets = filter_tweets_for_good_words(new_tweets)
+    presenters = filter_tweets_for_presenters(new_tweets)
+    presenters = clean_results(presenters, bad_names)
+    presenters = pick_best_presenters(presenters)
+    return presenters
+
 
 # Step 1: Get the time interval for the start of the award
 # Step 2: Find the tweets in that time interval
@@ -54,18 +64,20 @@ def filter_tweets_for_presenters(tweets):
 
 # Step 5: Return the names of the best presenters
 
-def presenters_for_award_alias(aliases):
-    
-    new_tweets = tweets_for_time_interval(tweets, aliases, min_before=2, min_after=1)
-    new_tweets = filter_tweets_for_good_words(new_tweets)
-    presenters = filter_tweets_for_presenters(new_tweets)
-    
-    return presenters
 
-def presenters_for_award(award):
-    award_name = standardize(award_name)
-    award_name_aliases = get_aliases(award_name)
-    return presenters_for_award_alias(award_name_aliases)
+
+def pick_best_presenters(presenters):
+    # TODO - pick the best presenters
+    if len(presenters) == 0:
+        return []
+    elif len(presenters) == 1:
+        return [list(presenters.keys())[0]]
+    elif len(presenters) == 2:
+        new_presenters = []
+        new_presenters.append(list(presenters.keys())[0])
+        if presenters[list(presenters.keys())[0]] * 0.6 < presenters[list(presenters.keys())[1]]:
+            new_presenters.append(list(presenters.keys())[0])
+        return new_presenters
 
 def clean_results(results, bad_names):
     keys = list(results.keys())
@@ -82,53 +94,32 @@ def clean_results(results, bad_names):
 
     results = new_results
     return results
-
-def AwardNameToPresenters():
-
-    award_aliases = json.load(open('award_aliases.json'))
-    list_of__all_aliases = []
-    alias_keys = list(award_aliases.keys())
-    tweets = preprocess(json.load(open(f'gg2013.json')))
-    results = {}
-    for alias_key in alias_keys:
-        results[alias_key] = presenters_for_award_alias(award_aliases[alias_key][1])
-    #Sort all of the inner dicts of results by the number of times they were mentioned
-    for key in results:
-        results[key] = dict(sorted(results[key].items(), key=lambda item: item[1], reverse=True))
+    
+# def AwardNameToPresenters(award: Award, tweets, winner_and_nominees):
+#     results = {}
+#     aliases = award.award_category.aliases
+#     presenters_for_award_alias(aliases, tweets, winner_and_nominees)
+    
+#     #Sort all of the inner dicts of results by the number of times they were mentioned
+#     for key in results:
+#         results[key] = dict(sorted(results[key].items(), key=lambda item: item[1], reverse=True))
             
 
-    # Step 1: Get the time interval for the start of the award
-    # Step 2: Find the tweets in that time interval
-    # Step 3: Filter those tweets to only include the tweets that mention the good words we found
-    # Step 4: Use NER to find the names of the presenters
-    # Step 5: Return the names of the presenters
+#     # Step 1: Get the time interval for the start of the award
+#     # Step 2: Find the tweets in that time interval
+#     # Step 3: Filter those tweets to only include the tweets that mention the good words we found
+#     # Step 4: Use NER to find the names of the presenters
+#     # Step 5: Return the names of the presenters
 
-    winner_and_nominees = set()
-    data = json.load(open('autograder/gg2013answers.json'))
-    keys = list(data['award_data'].keys())
-    data = data["award_data"]
+#     results = clean_results(results, winner_and_nominees)
 
-    for key in keys:
-        for nominee in data[key]["nominees"]:
-            winner_and_nominees.add(nominee)
-        winner_and_nominees.add(data[key]["winner"])
-
-    results = clean_results(results, winner_and_nominees)
-
-    newResult = {}
-    for result in results:
-        if len(result.keys()) == 1:
-            newResult[list(results[result].keys())[0]] = results[result][list(results[result].keys())[0]]
-            continue
-        if len(result > 1):
-            # If the first two presenters have more than 3 mentions, then we are done
-            # Add them to the new result
-            if results[result][list(results[result].keys())[1]] > 3
-                newResult[list(results[result].keys())[1]] = results[result][list(results[result].keys())[1]]
+#     presenters = []
+#     for key in results:
         
-    return results
+#     return results
 
 if __name__ == "__main__":
-    results = AwardNameToPresenters()
-    with open('results_new_new.json', 'w') as f:
+    tweets = preprocess(json.load(open('gg2013.json')))
+    results = AwardNameToPresenters(  )
+    with open('results_new.json', 'w') as f:
         json.dump(results, f, indent=4) 
