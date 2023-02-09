18ea5a08f49a2bc60e17128596372163b18c433b
Isaac Miller <isaacbmiller@gmail.com>
2023-02-08 18:58:18 -0600

    Add new award name to presenters
    Co-authored-by: Gabriel Bider <gabebider@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -23,6 +23,7 @@ def standardize(text):
     text = text.replace(",","")
     text = text.replace("-","")
     text = text.replace(".","")
+    text = text.replace("'", " ")
     text = text.replace(":","")
     text = text.replace("!", "")
     text = text.replace("?", "")
